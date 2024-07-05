f5ac258c51bbb7e6353bfda5a9f1e36ec90daefa
QUY666666 <lenhatquy2401@gmail.com>
2024-07-05 22:44:14 +0700

    update..

---
+++ js/index.js
@@ -0,0 +1,14 @@
+document.getElementById("heart_like").onclick = function() {
+    let heart = document.getElementById("heart_l");
+    heart.classList.toggle('like_shoe');
+}
+
+// document.getElementsByClassName("h_like").onclick = function() {
+//     let heart = document.getElementsByClassName("fa-heart")
+//     heart.classList.toggle('like_shoe');  
+// }
+
+// document.getElementById("heart_like").onclick = function() {
+//     let heart = document.getElementById("heart_like");
+//     heart.classList.toggle('likee');
+// }
