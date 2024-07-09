dbf8be5d52a9625e26a8fed6657d8370f8e20918
QUY666666 <lenhatquy2401@gmail.com>
2024-07-09 17:43:01 +0700

    update main detail

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+    //      details
 document.getElementById("heart_like").onclick = function() {
     let heart = document.getElementById("heart_l");
     heart.classList.toggle('like_shoe');
@@ -8,7 +10,6 @@ document.getElementById("heart_like").onclick = function() {
 //     heart.classList.toggle('like_shoe');  
 // }
 
-// document.getElementById("heart_like").onclick = function() {
-//     let heart = document.getElementById("heart_like");
-//     heart.classList.toggle('likee');
-// }
+
+
+
---
+++ js/indexdetail.js
@@ -0,0 +1,54 @@
+    
+    //          Axios  API
+    //      gọi api
+    // call product arrivals
+    //    https://shop.cyberlearn.vn/api/Product
+    function getInfoProduct() {
+      let promise = axios({
+        method: "GET",
+        url: "https://shop.cyberlearn.vn/api/Product",
+      });
+      promise
+        .then((res) => {
+          console.log(res);
+          const firstTenProducts = res.data.content.slice(0, 16);
+          renderProduct(firstTenProducts);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    
+    getInfoProduct();
+    
+    // Chức năng hiển thị thông tin sản phẩm lên giao diện
+    function renderProduct(arr) {
+      let content = "";
+      arr.forEach((item, index) => {
+        let { image, name, price } = item;
+        content += `
+        <div class="col-md-3 col-sm-6 mt-4">
+          <div class="product_item position-relative">
+            <img src="${image}" alt="${name}"/>
+            <h5 >${name}</h5>
+            <p class="mb-0">Types of Shoes</p>
+            <p class="mb-2">Quantity Color</p>
+            <div class="d-flex align-items-center product-footer text-center">
+            <button class="w-50 p-2 border-0">Buy now</button>
+            <div class="w-50 p-2">${price.toLocaleString("en-US", {
+              style: "currency",
+              currency: "USD",
+            })}</div>
+            </div>
+            <button class="btn-favorite position-absolute heart-icon"><span class="span-favorite"><i class="fa-regular fa-heart icon-favorite"></i></span></button>
+          </div>
+        </div>
+        `;
+      });
+      document.getElementById("product_item").innerHTML = content;
+    }
+
+// document.getElementById("product_item").innerHTML = "123456789";
+   
+    
+    
